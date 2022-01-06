856d54802b5ff0da35ddf8de8478000380e90358
Andi Liwantoro Susanto <liewantorosusanto.andi@gmail.com>
2022-01-06 17:36:00 +0700

    uhahah

--- src/main/java/com/bcafinance/engineapproval/controller/EngineApprovalController.java
+++ src/main/java/com/bcafinance/engineapproval/controller/EngineApprovalController.java
@@ -1,30 +1,52 @@
 package com.bcafinance.engineapproval.controller;
 
 import java.util.Date;
+import java.util.UUID;
 
 import com.bcafinance.engineapproval.model.LogApi;
 import com.bcafinance.engineapproval.repository.LogApiRepository;
 import com.bcafinance.engineapproval.request.ApprovalRequest;
 import com.bcafinance.engineapproval.response.ApprovalResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController @RequestMapping
 public class EngineApprovalController {
     
+    Logger logger = LoggerFactory.getLogger(EngineApprovalController.class);
+
     @Autowired
     LogApiRepository logApiRepository;
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @PostMapping("/approval")
-    ApprovalResponse approval(ApprovalRequest request) {
-        ApprovalResponse a = new ApprovalResponse();
-        LogApi logApi = new LogApi(null,"Tyes",null,1,"hm?",new Date(),null);
+    ApprovalResponse approval(@RequestBody ApprovalRequest request) {
+        ApprovalResponse response = new ApprovalResponse("00","sukses","1");
+        String token = UUID.randomUUID().toString();
+        LogApi logApi = new LogApi(null,request,null,1,1,new Date(),null);
         logApi = logApiRepository.save(logApi);
 
-        return new ApprovalResponse("test","tast","tust");
+        try {
+            logApi.setResJSON(objectMapper.writeValueAsString(response));
+            logApi.setUpdateddate(new Date());
+            logApiRepository.save(logApi);
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
+
+        request.
+
+        
+
+        return response;
     }
     
 }
